1eba33b22ab10733e70a6c924db5efbb8f756567
iamkira420 <lmboniswa01@gmail.com>
2026-04-13 14:39:52 +0200

    2nd try

--- hello-cube.js
+++ hello-cube.js
@@ -1,30 +1,26 @@
 import * as THREE from 'three';
 
 function main() {
-    const canvas = document.querySelector('#c');
-    const renderer = new THREE.WebGLRenderer({
-        antialias: true,
-        canvas
-    });
+  const canvas = document.querySelector('#c');
+  const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
 
-    const fov = 75;
-    const aspect = 2; // the canvas default
-    const near = 0.1;
-    const far = 5;
-    const camera =  new THREE.PerspectiveCamera(fov, aspect, near, far);
+  const fov = 75;
+  const aspect = 2;
+  const near = 0.1;
+  const far = 5;
 
-    const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.z = 2;
 
-    const boxWidth = 1;
-    const boxHeight = 1;
-    const boxDepth = 1;
-    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+  const scene = new THREE.Scene();
 
-    const material = new THREE.MeshBasicMaterial({color: 0x44aa88}); // greenish blue
-    const cube =  new THREE.Mesh(geometry, material);
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
 
-    scene.add(cube);
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
 
-    renderer.render(scene, camera);
+  renderer.render(scene, camera);
 }
 
+main();
