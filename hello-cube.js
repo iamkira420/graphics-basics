005cb42ed54b2e40faf4b8a946b669464ac52625
iamkira420 <lmboniswa01@gmail.com>
2026-04-13 15:04:33 +0200

    fixing animation

--- hello-cube.js
+++ hello-cube.js
@@ -1,26 +1,29 @@
 import * as THREE from 'three';
 
-function main() {
-  const canvas = document.querySelector('#c');
-  const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
+const canvas = document.querySelector('#c');
+const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
 
-  const fov = 75;
-  const aspect = 2;
-  const near = 0.1;
-  const far = 5;
+const fov = 75;
+const aspect = window.innerWidth / window.innerHeight;
+const near = 0.1;
+const far = 1000;
 
-  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.z = 2;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 2;
 
-  const scene = new THREE.Scene();
+const scene = new THREE.Scene();
 
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
 
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
-  renderer.render(scene, camera);
+function render(time) {
+    cube.rotation.x = time / 2000;
+    cube.rotation.y = time / 1000;
+
+    renderer.render(scene, camera);
 }
 
-main();
+renderer.setAnimationLoop(render);
