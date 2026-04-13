bbbb2b65e3cbd522fc7612b8146331000a93865a
iamkira420 <lmboniswa01@gmail.com>
2026-04-13 15:45:16 +0200

    added lighting

--- hello-cube.js
+++ hello-cube.js
@@ -14,10 +14,16 @@ camera.position.z = 2;
 const scene = new THREE.Scene();
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
+const material = new THREE.MeshPhongMaterial({ color: 0x44aa88 });
 
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+
+const color = 0xFFFFFF;
+const intensity = 3;
+const light = new THREE.DirectionalLight(color, intensity);
+light.position.set(-1, 2, 4);
+scene.add(light);
 
 function render(time) {
     cube.rotation.x = time / 2000;
